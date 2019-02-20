e7ce358e79ca0fc4720dc598c6d1daea35ea3a84
robin.sigmund@gmail.com <butterbrot1>
2019-02-20 09:26:24 +0100

    new recchaos texture

--- src/main/java/exordian_avenger/cards/RecurrentChaos.java
+++ src/main/java/exordian_avenger/cards/RecurrentChaos.java
@@ -20,7 +20,7 @@ public class RecurrentChaos extends CustomCard {
 	public static final String NAME = cardStrings.NAME;
 	public static final String DESCRIPTION = cardStrings.DESCRIPTION;
 	public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
-	public static final String IMG_PATH = "tea/img/cards/betaskill.png";
+	public static final String IMG_PATH = "tea/img/cards/recchaos.png";
 	private static final int COST = 0;
 
 	public RecurrentChaos() {

202ee6c80515a0dcbebf8ee479311e508f4dd55d
lobbienjonsji <butterbrot1>
2019-02-21 21:56:45 +0100

    new card texture

--- src/main/java/exordian_avenger/cards/HeroicStance.java
+++ src/main/java/exordian_avenger/cards/HeroicStance.java
@@ -22,7 +22,7 @@ public class HeroicStance extends CustomCard{
 	public static final String NAME = cardStrings.NAME;
 	public static final String DESCRIPTION = cardStrings.DESCRIPTION;
 	public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
-	public static final String IMG_PATH = "tea/img/cards/betaskill.png";
+	public static final String IMG_PATH = "tea/img/cards/heroicstance.png";
 	private static final int COST = 1;
 	public HeroicStance() {
 		super(ID, NAME, IMG_PATH, COST, DESCRIPTION, AbstractCard.CardType.SKILL, AbstractCardEnum.EX_DARK_RED,

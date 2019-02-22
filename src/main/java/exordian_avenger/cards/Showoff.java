4899f250c7b1544686d1e3a9a8f7ee672d0318d0
lobbienjonsji <butterbrot1>
2019-02-22 21:17:01 +0100

    new card sh

---
+++ src/main/java/exordian_avenger/cards/Showoff.java
@@ -0,0 +1,47 @@
+package exordian_avenger.cards;
+
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import basemod.abstracts.CustomCard;
+import com.megacrit.cardcrawl.powers.StrengthPower;
+import com.megacrit.cardcrawl.powers.VulnerablePower;
+import exordian_avenger.patches.AbstractCardEnum;
+
+public class Showoff extends CustomCard{
+    public static final String ID = "exordian_avenger:showoff";
+    private static CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+    public static final String NAME = cardStrings.NAME;
+    public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+    public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
+    public static final String IMG_PATH = "tea/img/cards/betaskill.png";
+    private static final int COST = 1;
+    public Showoff() {
+        super(ID, NAME, IMG_PATH, COST, DESCRIPTION, AbstractCard.CardType.SKILL, AbstractCardEnum.EX_DARK_RED,
+                AbstractCard.CardRarity.UNCOMMON, CardTarget.SELF_AND_ENEMY);
+        this.baseMagicNumber = 2;
+        this.isEthereal = true;
+        this.magicNumber = this.baseMagicNumber;
+    }
+
+    @Override
+    public void upgrade() {
+        upgradeName();
+        upgradeMagicNumber(1);
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        AbstractDungeon.actionManager.addToTop(new GainEnergyAction(this.magicNumber));
+        AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(m, p, new StrengthPower(m, 1), 1));
+        AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(m, p, new VulnerablePower(m, 1, false), 1));
+    }
+}

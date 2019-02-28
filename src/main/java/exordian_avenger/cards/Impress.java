725085f42a08ce6c581dff7fd6f0d08831c17dac
lobbienjonsji <butterbrot1>
2019-02-28 17:20:45 +0100

    Impress is now rare

--- src/main/java/exordian_avenger/cards/Impress.java
+++ src/main/java/exordian_avenger/cards/Impress.java
@@ -27,7 +27,7 @@ public class Impress extends CustomCard {
 
     public Impress() {
         super(ID, NAME, IMG_PATH, COST, DESCRIPTION, AbstractCard.CardType.ATTACK, AbstractCardEnum.EX_DARK_RED,
-                AbstractCard.CardRarity.COMMON, AbstractCard.CardTarget.ENEMY);
+                AbstractCard.CardRarity.UNCOMMON, AbstractCard.CardTarget.ENEMY);
         this.baseBlock = 4;
         this.exhaust = true;
     }
